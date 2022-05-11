1fd7db9e58d04eae8f32238d3a493df290c3b95e
pixgel <pixgel13040@gmail.com>
2022-05-10 22:57:13 -0500

    first commit

---
+++ src/main/java/com/example/chatarra/ChatarraApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatarra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatarraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatarraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatarra/ChatarraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatarra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatarraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
