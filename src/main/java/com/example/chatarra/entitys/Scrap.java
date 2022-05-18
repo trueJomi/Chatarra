0f32364a949f3db6744972f98ec46a424d561a88
kevinV13 <kevinandresvega13@gmail.com>
2022-05-17 22:04:16 -0500

    Desarrollo de la entidad Scrap

--- src/main/java/com/example/chatarra/entitys/Scrap.java
+++ src/main/java/com/example/chatarra/entitys/Scrap.java
@@ -1,9 +1,31 @@
 package com.example.chatarra.entitys;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "chatarra")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Scrap {
 
-    Integer idChatarra;
-    String type;
-    String name;
-    Float basePrice;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idChatarra;
+    @Column( name = "tipo",nullable = false)
+    private String type;
+    @Column( name = "nombre",nullable = false)
+    private String name;
+    @Column( name = "precioBase",nullable = false)
+    private Float basePrice;
+    @ManyToOne
+    @JoinColumn( name = "idSeller", foreignKey = @ForeignKey(name = "FK_Chatarra_Vendedor"))
+    private Seller seller;
+
+
 }
