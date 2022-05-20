8c5008ef302632946ea85a4beafdba41b550b52c
Gean <92776561+GeanBravo@users.noreply.github.com>
2022-05-20 18:56:24 -0500

    Update Schedule.java

--- src/main/java/com/example/chatarra/entitys/Schedule.java
+++ src/main/java/com/example/chatarra/entitys/Schedule.java
@@ -1,4 +1,29 @@
 package com.example.chatarra.entitys;
 
-public class Schedule {
-}
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "Schedule")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Scrap {
+
+	  @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idDate
+
+    @Column(name = "StartDate", nullable = false)
+    private Date startDate;
+
+    @Column(name = "EndDate", nullable = false)
+    private Date endDate;
+
+    @Column(name = "DayWeek", nullable = false)
+    private Date dayWeek; 
+ }
